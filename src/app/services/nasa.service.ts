39cfb2b8c1d706205e0b33e6e49aa3c7a63cf0e9
Alberto Ruiz <ruizserranoalberto5@gmail.com>
2022-04-16 19:41:40 +0200

    Cambiadas las variables de entorno utilizadas en nasaService y añadido el rango de fechas

--- src/app/services/nasa.service.ts
+++ src/app/services/nasa.service.ts
@@ -1,16 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FotoNasa } from '../interfaces/fotoNasa.interface';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
+import { formatDate } from '@angular/common';
+import localeES from "@angular/common/locales/es";
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeES, "es");
 
 @Injectable({
   providedIn: 'root',
 })
 export class NasaService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
+  get httpParams() {
+    let fechaInicio = new Date()
+    fechaInicio.setDate(new Date().getDate() - 5)
+    let fechaFormateada = formatDate(fechaInicio, "YYYY-MM-dd", "es")
+    return new HttpParams().set('api_key', environment.apiKey).set("start_date", fechaFormateada).set("end_date", "");
+  }
 
   obtenerFotosNasa(): Observable<FotoNasa[]> {
-    return this.http.get<FotoNasa[]>(`${environment.apiUrl}`);
+    return this.http.get<FotoNasa[]>(`${environment.apiUrl}`, { params: this.httpParams });
   }
 }
