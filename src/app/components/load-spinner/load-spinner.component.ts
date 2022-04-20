641f9855dfb3676b44584333ba69e730d9462627
Alberto34315 <ruizserranoalberto5@gmail.com>
2022-04-20 10:42:21 +0200

    El spinner infinito está solucionado

--- src/app/components/load-spinner/load-spinner.component.ts
+++ src/app/components/load-spinner/load-spinner.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-load-spinner',
   templateUrl: './load-spinner.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoadSpinnerComponent implements OnInit {
-
-  constructor() { }
+  public mostrarLoad = true;
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.mostrarLoad = false;
+    }, 5000);
   }
-
 }
